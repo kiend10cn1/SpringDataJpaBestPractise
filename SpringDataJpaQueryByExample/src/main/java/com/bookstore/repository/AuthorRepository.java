c4703551d7811e8805f03e4687922dfcb567fa11
xoandaica <kiend10cn1@gmail.com>
2021-09-05 15:21:23 +0700

    Add SpringDataJpaQueryByExample

---
+++ SpringDataJpaQueryByExample/src/main/java/com/bookstore/repository/AuthorRepository.java
@@ -0,0 +1,14 @@
+package com.bookstore.repository;
+
+import com.bookstore.entity.Author;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+
+}
